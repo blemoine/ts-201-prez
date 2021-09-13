d9b053b1b695e9001d67f17dbedc7dc994c40395
Benoit Lemoine <benoit.lemoine@decathlon.com>
2021-09-13 13:08:30 -0400

    feat: add some content

--- implem/src/index.ts
+++ implem/src/index.ts
@@ -1,3 +1,19 @@
-function length(p: string | null): number {
-    return p.length;
-}                    
+type Admin = { id:string, kind: 'admin' };
+type SuperAdmin = { id:string, kind: 'superadmin', age: number };
+type UltraAdmin = { id:string, kind: 'ultradmin', birthDate: Date };
+type User = Admin | SuperAdmin | UltraAdmin
+
+
+function cannotHappen(x: never): never {
+    throw new Error(`${x} is not a valid value`);
+}
+
+function getAge(user: User): number | null {
+    if (user.kind === 'superadmin') {
+        return user.age;
+    } else if(user.kind === 'admin') {
+        return null;
+    } else {
+        cannotHappen(user);
+    }
+}
